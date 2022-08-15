a45e5ae76df87f604de953c765a2f91e4a11540b
Kasra Sharifi <sharifi.kasra72@gmail.com>
2022-08-15 14:44:19 -0500

    0.1 /$ promo_tag is added

--- multibuy_finder.py
+++ multibuy_finder.py
@@ -1,6 +1,8 @@
 def multibuy_finder(promo_tag):
     if promo_tag.find("WYB") >= 0:
         print("MustBuy")
+    elif promo_tag.find("/$"):
+        print("MustBuy")
     else:
         print("Temporary Price Reduction")
 
